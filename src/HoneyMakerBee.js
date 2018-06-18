62d1395a50def50e57c3f654cf233bb7ef087afe
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 13:10:05 -0700

    Add overwritten prototypes and new methods to HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,5 +1,17 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+};
 
+Object.create(Bee.prototype);
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
 };
 
 /*
