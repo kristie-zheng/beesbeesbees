ffe4c36e9b52859fdef6efb4b94f8e5130d134f5
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 12:18:15 -0700

    Notes and transformation steps

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,27 @@
 var ForagerBee = function() {
 };
 
+
+/*
+ Create a ForagerBee class, in pseudoclassical style, with:
+    made from the Bee superclass
+    set the prototype
+    set the constructor
+   [overwrite] age prop
+      set to 10
+   [overwrite] job property
+      set to find pollen
+   [overwrite/inherit] color property
+      inherited from bee that is set to yellow
+    [inherit] food property 
+      inherited from grub (which superclass bee also inherited)
+    [inherit] eat method 
+      inherited from grub
+      Obj.create on bee's prototype because it a;so inherited this from grub
+    [add] canfly property
+      set to true
+    [add] a treasureChest property 
+      set to []
+    [add] forage method 
+      adds to treasure chest. this is done on honeyMakerBee's prototype
+*/
