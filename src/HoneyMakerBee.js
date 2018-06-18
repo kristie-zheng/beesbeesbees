16a11784cd42cfa8c784d11f13d494f1b2d645a7
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 12:16:24 -0700

    Notes and transformation steps

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,27 @@
 var HoneyMakerBee = function() {
+
 };
 
+/*
+ Create a HoneyMakerBee class, in pseudoclassical style, with:
+    made from the Bee superclass
+    set the prototype
+    set the constructor
+   [overwrite] age prop
+      set to 10
+   [overwrite] job property
+      set to make honey
+   [overwrite/inherit] color property
+      inherited from bee that is set to yellow
+    [inherit] food property 
+      inherited from grub (which superclass bee also inherited)
+    [inherit] eat method 
+      inherited from grub
+      Obj.create on bee's prototype because it a;so inherited this from grub
+    [add] a honeyPot property 
+      set to 0
+    [add] makeHoney method 
+      adds 1 to that honeyBee\'s honeyPot. this is done on honeyMakerBee's prototype
+    [add] giveHoney method 
+      subtracts 1 from that honeyBee\'s honeyPot. this is done on honeyMakerBee's prototype
+*/
