6da0b9edad42dcaa089d2d1fb174e94e17df5d43
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 11:49:58 -0700

    Notes/transformation steps for bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,22 @@
 var Bee = function() {
+
 };
+
+
+/*
+Bee is a subclass of the grub superclass
+  use Grub.call so that a new instance of grub is created and this is bound the instance of bee 
+Bee subclass needs to have its own prototype where its specific methods are stored
+[overwrite] age prop
+  set to 5
+[overwrite] color prop
+  set to yellow
+[inherit] food prop
+  no need to do anything; failed lookup should delegate to grub superclass
+[inherit] eat method
+  no need to do anything; failed lookup should delegate to grub's methods
+[add] job method
+  add this to Bee prototype's method container
+*/
+
 
