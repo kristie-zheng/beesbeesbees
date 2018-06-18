d15fa3d536f73b9758b05dd45185439123127e97
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 11:36:54 -0700

    Created Grub superclass

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,11 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  Grub.prototype.eat = function() {
+    console.log('Ate ' + this.food);
+  }
+
 };
 
 /*grub is a superclass and does not derive from any superclass
