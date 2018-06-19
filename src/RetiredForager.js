0ad7d2d00abfcee82d91d9b0fe3eace5ae2c6d71
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 22:37:38 -0700

    Fix error where RetiredForagerBee was adding its methods to HoneyMakerBee's prototype

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,7 +1,24 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this); 
+    this.age = 40;
+    this.job = 'gamble';
+    this.color = 'grey';
+    this.canFly = false;
+    this.treasureChest = [];
+
 };
 
 
+
+
+RetiredForagerBee.prototype = Object.create(Bee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead";
+};
 /*
  Create a RetiredForagerBee class, in pseudoclassical style, with:
     made from the ForagerBee superclass
