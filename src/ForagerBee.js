234c8ef170ab321556a450bc7ef684bd03153d46
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-18 22:37:13 -0700

    Fix error where ForagerBee was calling RetiredForagerBee's methods

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,5 +1,20 @@
 var ForagerBee = function() {
+  Bee.call(this); 
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  console.log(12)
+  this.treasureChest.push(treasure);
+};
+
+
+
 
 
 /*
